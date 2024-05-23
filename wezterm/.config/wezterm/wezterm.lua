71c4727f3422f98f0a02bb9c859bd9e6ab3f4483
duylp <duy7797@gmail.com>
2024-05-24 01:00:13 +0700

    feat: add keymap for multiplexing

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -18,6 +18,7 @@ config.color_scheme = "OneDark (base16)"
 
 -- Key bindings
 config.disable_default_key_bindings = true
+config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 3000 }
 
 local act = wezterm.action
 
@@ -27,8 +28,19 @@ config.keys = {
 	{ mods = "CTRL", key = "=", action = act.IncreaseFontSize },
 	{ mods = "CTRL", key = "-", action = act.DecreaseFontSize },
 	{ mods = "CTRL", key = "0", action = act.ResetFontSize },
-	--  {mods="CTRL", key="p", action=act.ActivateCommandPalette},
+	{ mods = "LEADER", key = "p", action = act.ActivateCommandPalette },
 	{ mods = "CTRL", key = "f", action = act.Search({ CaseSensitiveString = "" }) },
+	-- multiplexing keymap
+	{ key = "%", mods = "LEADER|SHIFT", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = '"', mods = "LEADER|SHIFT", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "h", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "j", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "k", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "l", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Right", 5 }) },
 }
 
 -- and finally, return the configuration to wezterm
