4705437a28f902c7ae3194ebad689a8b7a2f6e55
duylp <duy7797@gmail.com>
2025-10-22 23:14:29 +0800

    feat(nvim): customize neo tree config

--- nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -12,9 +12,21 @@ return {
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    { '<leader>gg', ':Neotree float git_status<CR>', { desc = 'Git status (Neo-tree)' } },
   },
   opts = {
+    window = {
+      -- position = 'float',
+    },
     filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          '.git',
+        },
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
